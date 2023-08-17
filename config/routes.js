40309cb39fffb68f53194e36b11341a6ce678365
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-08-16 21:48:26 -0300

    Método DELETE e teste com Postman

--- config/routes.js
+++ config/routes.js
@@ -2,17 +2,17 @@ const express = require('express')
 const routes = express.Router()
 
 let db = [
-    { "\"id\"": 1, "\"Nome\"": "Starla Musicka", "\"Data de Nascimento\"": "04/09/1988", "\"Sexo\"": "F"}, 
-    { "\"id\"": 2, "\"Nome\"": "Cherice Fellgett", "\"Data de Nascimento\"": "04/02/1959", "\"Sexo\"": "M"},
-    { "\"id\"": 3, "\"Nome\"": "Nathan Hacquel", "\"Data de Nascimento\"": "08/07/1966", "\"Sexo\"": "M"},
-    { "\"id\"": 4, "\"Nome\"": "Sonny Gladdor", "\"Data de Nascimento\"": "28/02/1988", "\"Sexo\"": "F"},
-    { "\"id\"": 5, "\"Nome\"": "Dean Adrienne", "\"Data de Nascimento\"": "16/06/1987", "\"Sexo\"": "M"},
-    { "\"id\"": 6, "\"Nome\"": "Ardelis Sarten", "\"Data de Nascimento\"": "03/02/1980", "\"Sexo\"": "M"},
-    { "\"id\"": 7, "\"Nome\"": "Krishnah Dane", "\"Data de Nascimento\"": "07/08/1968", "\"Sexo\"": "M"},
-    { "\"id\"": 8, "\"Nome\"": "Flori Hynes", "\"Data de Nascimento\"": "20/09/1968", "\"Sexo\"": "M"},
-    { "\"id\"": 9, "\"Nome\"": "Maighdiln Fiander", "\"Data de Nascimento\"": "05/02/2019", "\"Sexo\"": "M"},
-    { "\"id\"": 10, "\"Nome\"": "Colet Onslow", "\"Data de Nascimento\"": "23/12/2009", "\"Sexo\"": "F"}
-]
+    { "id": 1, "Nome": "Starla Musicka", "Data de Nascimento": "04/09/1988", "Sexo": "F" },
+    { "id": 2, "Nome": "Cherice Fellgett", "Data de Nascimento": "04/02/1959", "Sexo": "M" },
+    { "id": 3, "Nome": "Nathan Hacquel", "Data de Nascimento": "08/07/1966", "Sexo": "M" },
+    { "id": 4, "Nome": "Sonny Gladdor", "Data de Nascimento": "28/02/1988", "Sexo": "F" },
+    { "id": 5, "Nome": "Dean Adrienne", "Data de Nascimento": "16/06/1987", "Sexo": "M" },
+    { "id": 6, "Nome": "Ardelis Sarten", "Data de Nascimento": "03/02/1980", "Sexo": "M" },
+    { "id": 7, "Nome": "Krishnah Dane", "Data de Nascimento": "07/08/1968", "Sexo": "M" },
+    { "id": 8, "Nome": "Flori Hynes", "Data de Nascimento": "20/09/1968", "Sexo": "M" },
+    { "id": 9, "Nome": "Maighdiln Fiander", "Data de Nascimento": "05/02/2019", "Sexo": "M" },
+    { "id": 10, "Nome": "Colet Onslow", "Data de Nascimento": "23/12/2009", "Sexo": "F" }
+];
 //rota defaut(nenhum nome depois da porta)
 //requisição e resposta que retornará o conteúdo da variável db
 routes.get('/', (req, res) => {
@@ -28,6 +28,20 @@ routes.post('/add', (req, res) => {
     db.push(body)
     return res.json(body)
 })
+//Simular o Metodo delete. Será alterado quando for usado um banco de dados real.
+routes.delete('/:id', (req, res) => {
+    const id = req.params.id
+//A função "filter" filtra os registros de acordo com o id informado na rota
+//se "!==" não for igual, será mantido no novo array(newDB). Se for igual,
+//será excluído do novo array(filtrado)
+    let newDB = db.filter(registro => {
+        return registro["id"] !== parseInt(id);
+    })
 
+    db = newDB
+
+    return res.json(newDB)
+
+})
 
 module.exports = routes
--- index.js
+++ index.js
@@ -14,34 +14,7 @@ app.use(express.json())
 app.use(cors())
 app.use(routes)
 
-/*Rotas movidas para config/routes
-let db = [
-    { "\"id\"": 1, "\"Nome\"": "Starla Musicka", "\"Data de Nascimento\"": "04/09/1988", "\"Sexo\"": "F"}, 
-    { "\"id\"": 2, "\"Nome\"": "Cherice Fellgett", "\"Data de Nascimento\"": "04/02/1959", "\"Sexo\"": "M"},
-    { "\"id\"": 3, "\"Nome\"": "Nathan Hacquel", "\"Data de Nascimento\"": "08/07/1966", "\"Sexo\"": "M"},
-    { "\"id\"": 4, "\"Nome\"": "Sonny Gladdor", "\"Data de Nascimento\"": "28/02/1988", "\"Sexo\"": "F"},
-    { "\"id\"": 5, "\"Nome\"": "Dean Adrienne", "\"Data de Nascimento\"": "16/06/1987", "\"Sexo\"": "M"},
-    { "\"id\"": 6, "\"Nome\"": "Ardelis Sarten", "\"Data de Nascimento\"": "03/02/1980", "\"Sexo\"": "M"},
-    { "\"id\"": 7, "\"Nome\"": "Krishnah Dane", "\"Data de Nascimento\"": "07/08/1968", "\"Sexo\"": "M"},
-    { "\"id\"": 8, "\"Nome\"": "Flori Hynes", "\"Data de Nascimento\"": "20/09/1968", "\"Sexo\"": "M"},
-    { "\"id\"": 9, "\"Nome\"": "Maighdiln Fiander", "\"Data de Nascimento\"": "05/02/2019", "\"Sexo\"": "M"},
-    { "\"id\"": 10, "\"Nome\"": "Colet Onslow", "\"Data de Nascimento\"": "23/12/2009", "\"Sexo\"": "F"}
-]
-//rota defaut(nenhum nome depois da porta)
-//requisição e resposta que retornará o conteúdo da variável db
-app.get('/', (req, res) => {
-    return res.json(db)
-})
-//Criar o corpo da requisição
-app.post('/add', (req, res) => {
-    const body = req.body 
-
-    if (!body)
-        return res.status(400).end()
-
-    db.push(body)
-    return res.json(body)
-})*/
+//Rotas movidas para config/routes
 
 app.listen(8090, () => {
     console.log(`Express started at http://localhost:8090`)
